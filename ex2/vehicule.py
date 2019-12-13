a5b2fa7e9bf2c70051c9299de14a198cc3d9de77
Kevin <kevin.billet.pro@gmail.com>
2019-12-13 10:05:57 +0100

    add commentary

--- ex2/vehicule.py
+++ ex2/vehicule.py
@@ -3,12 +3,13 @@ class Vehicule():
     this class has one method class who check if the vehicule_registration has a good format """
 
     def __init__(self):
-        """"""
+        """we add two arguments vehicule_registration and color"""
         self.vehicle_registration = vehicle_registration
         self.color = color
 
     @classmethod
     def check_vehicle_registration(cls,vehicule_registration):
+        """this method check if vehicule_registration has a good format """
         cls.expression="123456"
         cls.vehicle_registration = vehicule_registration
         if len(cls.expression) == len(cls.vehicle_registration) and cls.expression.isalnum() and cls.vehicule_registration.isalnum() :
